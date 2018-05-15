75a0d4cb8ee2c1fbc1f362273ebed5b1afc2ac21
Adam Quinlan <aquinlan@tilsontech.com>
2018-05-15 14:07:49 -0400

    Initialized vuepress tilson portfolio

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Tilson Development Projects',
+    description: 'A space to record past development projects',
+    configurewebpack: {
+        resolve: {
+          alias: {
+            '@media': 'img'
+          }
+        }
+      }
+};
